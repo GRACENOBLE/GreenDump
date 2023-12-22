e44f78ab780a5b5f20a3114c071aa087c6e79c0c
GRACENOBLE <gracenoble72@gmail.com>
2023-12-22 21:38:02 +0300

    Adding fonts to resource folder

--- app/src/main/java/com/example/greendump/landingPage.kt
+++ app/src/main/java/com/example/greendump/landingPage.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -30,7 +33,8 @@ import androidx.navigation.compose.rememberNavController
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 @Composable
 fun LandingPage (navController: NavController){
@@ -122,19 +126,57 @@ fun LandingPageHeader (){
         bottomEnd = 120.dp,
     )
 
-    Box {
-        Surface (
-            modifier  = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
-                .clip(cornerShape),
-            color = Color(0xFF2e8355)
+    Surface (
+        modifier  = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .clip(cornerShape),
+        color = Color(0xFF2e8355)
+    ){
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
         ){
+            Box (
+                modifier = Modifier
+                    .height(230.dp)
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.recycle_icon),
+                    contentDescription = stringResource(R.string.recycle_icon),
+                    modifier = Modifier
+                        .size(200.dp)
+                        .align(alignment = Alignment.TopCenter)
+                )
+
+                Row (
+                    modifier = Modifier
+                        .align(alignment = Alignment.BottomCenter)
+
+                ){
+                    Text(
+                        text = "GREEN",
+                        style = TextStyle(
+                            fontSize = 50.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .width(10.dp)
+                    )
+                    Text(
+                        text = "DUMP",
+                        style = TextStyle(
+                            fontSize = 50.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        )
+                    )
+                }
+            }
         }
-        Text(
-            text = stringResource(R.string.welcomeMessage),
-            style = TextStyle(fontSize = 40.sp)
-        )
     }
 }
 
