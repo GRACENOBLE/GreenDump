d871f9389711b24d43f306fc4c8a6118db908980
GRACENOBLE <gracenoble72@gmail.com>
2023-12-27 22:06:53 +0300

    Adding header to location page

--- app/src/main/java/com/example/greendump/LocationPage.kt
+++ app/src/main/java/com/example/greendump/LocationPage.kt
@@ -2,26 +2,33 @@ package com.example.greendump
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +41,7 @@ fun LocationPage(){
         modifier = Modifier
             .fillMaxSize()
     ){
-        LocationIcon()
+        LocationPageHeader()
         LocationText()
         Map()
         ConfirmButton()
@@ -44,31 +51,120 @@ fun LocationPage(){
 @Composable
 fun LocationPageBackground (){
     Surface (
-        modifier = Modifier
-            .alpha(0.5F)
+        color = Color.White
     ){
-        Image(
-            painter = painterResource(id = R.drawable.background),
-            contentScale = ContentScale.Crop,
-            alpha = 0.2F,
-            contentDescription = stringResource(id = R.string.background_image)
-        )
     }
 }
 
 @Composable
-fun LocationIcon(){
-    Row (
-        horizontalArrangement = Arrangement.Center,
+fun LocationPageHeader (){
+
+    val poppins = FontFamily(
+        Font(R.font.poppins_black , FontWeight.Black),
+        Font(R.font.poppins_blackitalic , FontWeight.Black),
+        Font(R.font.poppins_bold , FontWeight.Bold),
+        Font(R.font.poppins_bolditalic , FontWeight.Bold),
+        Font(R.font.poppins_extrabold , FontWeight.ExtraBold),
+        Font(R.font.poppins_extrabolditalic , FontWeight.ExtraBold),
+        Font(R.font.poppins_extralight , FontWeight.ExtraLight),
+        Font(R.font.poppins_extralightitalic , FontWeight.ExtraLight),
+        Font(R.font.poppins_italic , FontWeight.Normal),
+        Font(R.font.poppins_light , FontWeight.Light),
+        Font(R.font.poppins_lightitalic , FontWeight.Light),
+        Font(R.font.poppins_medium , FontWeight.Medium),
+        Font(R.font.poppins_mediumitalic , FontWeight.Medium),
+        Font(R.font.poppins_regular , FontWeight.Normal),
+        Font(R.font.poppins_thin , FontWeight.Thin),
+        Font(R.font.poppins_thinitalic, FontWeight.Thin),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold),
+        Font(R.font.poppins_semibolditalic, FontWeight.SemiBold)
+    )
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
+            .height(60.dp),
     ){
-        Image(
-            painter = painterResource(id = R.drawable.google_maps),
-            contentDescription = stringResource(R.string.google_maps_icon),
+
+        Surface (//back button
+            color = Color(0xFF2e8355),
             modifier = Modifier
-                .size(80.dp)
-        )
+                .fillMaxHeight()
+                .width(80.dp)
+                .clip(
+                    RoundedCornerShape(
+                        bottomEnd = 20.dp
+                    )
+                )
+        ){
+            Box (
+                Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.left),
+                    contentDescription = "Back button",
+                    Modifier
+                        .size(40.dp)
+
+                )
+            }
+        }
+
+        Surface (
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .fillMaxHeight()
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 20.dp,
+                        bottomEnd = 20.dp
+                    )
+                )
+        ){
+            Text(
+                text = "Location:",
+                style = TextStyle(
+                    fontSize = 40.sp,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White
+                ),
+                modifier = Modifier
+                    .padding(
+                        start = 20.dp,
+                        end = 10.dp
+                    )
+            )
+        }
+
+        Surface (//back button
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(80.dp)
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 20.dp
+                    )
+                )
+        ){
+            Box (
+                Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "Back button",
+                    Modifier
+                        .size(40.dp)
+
+                )
+            }
+        }
     }
 }
 
@@ -98,7 +194,8 @@ fun Map(){
                 end = 20.dp,
                 bottom = 50.dp
             )
-            .height(400.dp),
+            .height(400.dp)
+            ,
         contentScale = ContentScale.Crop
 
     )
