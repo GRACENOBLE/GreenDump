7a6d55d55765be417f044eb4d614fc1f4d829ae4
GRACENOBLE <gracenoble72@gmail.com>
2024-01-02 16:09:30 +0300

    modifying Service prices page to look more like design implementation

--- app/src/main/java/com/example/greendump/ServicePricesPage.kt
+++ app/src/main/java/com/example/greendump/ServicePricesPage.kt
@@ -2,9 +2,11 @@ package com.example.greendump
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +24,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,21 +35,31 @@ import androidx.compose.ui.unit.sp
 fun ServicePricesPage(){
     ServicePricesBackground()
     Column (
-        verticalArrangement = Arrangement.SpaceAround,
+        verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
             .fillMaxSize()
     ){
-        Column {
-            Tier()
+        ServicePricesHeader()
 
-            Spacer(
-                modifier = Modifier
-                    .height(40.dp)
-            )
+        Column (
+            verticalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+            .fillMaxHeight()
+        ){
+            Column {
+                Tier()
 
-            Package()
+                Spacer(
+                    modifier = Modifier
+                        .height(40.dp)
+                )
+
+                Package()
+            }
+
+            Charge()
         }
-        Charge()
+
     }
 }
 
@@ -59,55 +74,153 @@ fun ServicePricesBackground (){
 }
 
 @Composable
-fun Tier(){
-    Surface(
-        color = Color(0xFF2e8355),
+fun ServicePricesHeader(){
+    val poppins = FontFamily(
+        Font(R.font.poppins_black , FontWeight.Black),
+        Font(R.font.poppins_blackitalic , FontWeight.Black),
+        Font(R.font.poppins_bold , FontWeight.Bold),
+        Font(R.font.poppins_bolditalic , FontWeight.Bold),
+        Font(R.font.poppins_extrabold , FontWeight.ExtraBold),
+        Font(R.font.poppins_extrabolditalic , FontWeight.ExtraBold),
+        Font(R.font.poppins_extralight , FontWeight.ExtraLight),
+        Font(R.font.poppins_extralightitalic , FontWeight.ExtraLight),
+        Font(R.font.poppins_italic , FontWeight.Normal),
+        Font(R.font.poppins_light , FontWeight.Light),
+        Font(R.font.poppins_lightitalic , FontWeight.Light),
+        Font(R.font.poppins_medium , FontWeight.Medium),
+        Font(R.font.poppins_mediumitalic , FontWeight.Medium),
+        Font(R.font.poppins_regular , FontWeight.Normal),
+        Font(R.font.poppins_thin , FontWeight.Thin),
+        Font(R.font.poppins_thinitalic, FontWeight.Thin),
+        Font(R.font.poppins_semibold, FontWeight.SemiBold),
+        Font(R.font.poppins_semibolditalic, FontWeight.SemiBold)
+    )
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(end = 20.dp)
-            .clip(
-                RoundedCornerShape(
-                    topEnd = 40.dp,
-                    bottomEnd = 40.dp
+            .height(60.dp),
+    ){
+
+        Surface (//back button
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(80.dp)
+                .clip(
+                    RoundedCornerShape(
+                        bottomEnd = 20.dp
+                    )
+                )
+        ){
+            Box (
+                Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.left),
+                    contentDescription = "Back button",
+                    Modifier
+                        .size(40.dp)
 
                 )
-            )
-            .height(70.dp)
-    ) {
-        Row(
+            }
+        }
+
+        Surface (
+            color = Color(0xFF2e8355),
             modifier = Modifier
-                .fillMaxWidth()
-        ) {
+                .fillMaxHeight()
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 20.dp,
+                        bottomEnd = 20.dp
+                    )
+                )
+        ){
             Text(
-                text = stringResource(R.string.tier),
-                style = TextStyle(fontSize = 30.sp),
+                text = "Packages:",
+                style = TextStyle(
+                    fontSize = 35.sp,
+                    fontFamily = poppins,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White
+                ),
                 modifier = Modifier
-                    .padding(start = 10.dp)
+                    .padding(
+                        start = 20.dp,
+                        end = 10.dp
+                    )
             )
+        }
 
-            Spacer(
-                modifier = Modifier
-                    .width(50.dp)
-            )
+        Surface (//back button
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(80.dp)
+                .clip(
+                    RoundedCornerShape(
+                        bottomStart = 20.dp
+                    )
+                )
+        ){
+            Box (
+                Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "Back button",
+                    Modifier
+                        .size(40.dp)
 
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Tier(){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.tier),
+            style = TextStyle(fontSize = 30.sp),
+            modifier = Modifier
+                .padding(start = 10.dp)
+        )
+
+        Spacer(
+            modifier = Modifier
+                .width(50.dp)
+        )
+
+        Surface(
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 40.dp,
+                        bottomStart = 40.dp
+                    )
+                )
+                .height(70.dp)
+
+        ) {
             Row(
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.premium),
-                    contentDescription = stringResource(id = R.string.premium_icon),
-                    modifier = Modifier
-                        .size(50.dp)
-                )
-
-                Spacer(
-                    modifier = Modifier
-                        .width(10.dp)
-                )
-
                 Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically,
@@ -118,7 +231,7 @@ fun Tier(){
                         text = stringResource(id = R.string.premium),
                         style = TextStyle(fontSize = 30.sp),
                         modifier = Modifier
-                            .padding(end = 10.dp)
+                            .padding(start = 50.dp)
                     )
 
                     Image(
@@ -130,17 +243,18 @@ fun Tier(){
                     )
                 }
             }
-
         }
+
     }
 }
 
 @Composable
 fun Package(){
-    Row (
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-    ){
+    ) {
         Text(
             text = stringResource(R.string.pack),
             style = TextStyle(fontSize = 30.sp),
@@ -153,28 +267,35 @@ fun Package(){
                 .width(50.dp)
         )
 
-        Surface (
-            modifier = Modifier,
-            color = Color.White
+        Surface(
+            color = Color(0xFF2e8355),
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 40.dp,
+                        bottomStart = 40.dp
+                    )
+                )
+                .height(70.dp)
 
-        ){
-            Row (
+        ) {
+            Row(
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-            ){
-                Row (
+            ) {
+                Row(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                ){
+                ) {
                     Text(
-                        text = stringResource(R.string.premium_1),
+                        text = stringResource(id = R.string.premium),
                         style = TextStyle(fontSize = 30.sp),
                         modifier = Modifier
-                            .padding(end = 10.dp)
+                            .padding(start = 50.dp)
                     )
 
                     Image(
@@ -203,11 +324,12 @@ fun Charge(){
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(end = 20.dp)
-                .clip(RoundedCornerShape(
-                    topEnd = 40.dp,
-                    bottomEnd = 40.dp
+                .clip(
+                    RoundedCornerShape(
+                        topEnd = 40.dp,
+                        bottomEnd = 40.dp
 
-                )
+                    )
                 )
                 .height(70.dp)
         ) {
